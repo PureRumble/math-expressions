109a42577899b8381bb3747024dc6d9edcf17c36
Arash Bazrafshan <arashu@gmail.com>
2020-07-06 17:22:05 +0200

    Remove unnecessary alias type in class Variable.
    Add documentation for class Variable and its memb funcs.

--- variable.hpp
+++ variable.hpp
@@ -9,21 +9,29 @@
 namespace ImintMath
 {
 
+/* Class instance represents a variable in a mathematical expression.
+For instance new Addition( new Variable( "varA" ), new Constant( 7 ) )
+is varA+7.
+
+Variable instances in an Expression instance can then
+be replaced by constant values by using Expression::simplify() or
+Expression::evaluate() together with an Expression::VariableMap. */
 class Variable : public Expression
 {
   private:
     const std::string name;
 
   public:
-    using VariableMap =
-      std::unordered_map< Variable, int64_t >;
-
     Variable( const std::string name );
 
     virtual Expression* copy() const override;
 
     virtual std::string getStrRepr( bool withParantheses ) const override;
 
+/* Returns a new Expression instance. If the current Variable's name exists
+in the arg map then the corresponding variable value is used to create a
+new Constant and it is returned. Otherwise a copy of this Variable is
+returned. */
     virtual Expression* simplifyRec( const Expression::VariableMap& map )
     const override;
 
@@ -33,3 +41,4 @@ class Variable : public Expression
 }
 
 #endif
+
