bcd6c6726316611894e13289d802094b5be528d4
Arash Bazrafshan <arashu@gmail.com>
2020-07-06 19:29:47 +0200

    Add description of tests.
    Remake some tests. Delete one test since it was unnecessary.
    
    All tests succeed.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "variable.hpp"
 #include "binary_operator.hpp"
 #include "addition.hpp"
+#include "multiplication.hpp"
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
@@ -20,6 +21,7 @@ int main()
 
   assert( constant->toString() == "77" );
 
+// Simplifying a Constant returns a new Constant with same value.
   Constant* constEval = dynamic_cast<Constant*>( constant->simplify() );
 
   assert( constEval != nullptr );
@@ -31,18 +33,26 @@ int main()
   delete constEval;
 
 
+
   Variable* variable = new Variable( "TestVarName" );
 
   assert( variable->getName() == "TestVarName" );
 
   assert( variable->toString() == "TestVarName" );
 
+// Simplifying a Variable returns a new Variable with same name.
+// A Constant is not returned since no value was specified for
+// variable when calling simplify().
   Variable* varEval = dynamic_cast<Variable*>( variable->simplify() );
 
   assert( varEval != nullptr );
   assert( varEval != variable );
-  assert( variable->getName() == "TestVarName" );
+  assert( varEval->getName() == "TestVarName" );
 
+  delete varEval;
+
+// Evaluating a Variable without specifying a value for it throws
+// a runtime_error.
   bool excThrown = false;
 
   try
@@ -56,6 +66,8 @@ int main()
 
   assert( excThrown == true );
 
+// Simplifying a Variable when a new value for it has been specified
+// gives a new Constant with that value.
   Expression::VariableMap varMap( { { "TestVarName", 55 } } );
 
   constEval = dynamic_cast<Constant*>( variable->simplify( varMap ) );
@@ -63,12 +75,12 @@ int main()
   assert( constEval != nullptr );
   assert( constEval->getValue() == 55 );
 
-  delete varEval;
   delete constEval;
   delete variable;
 
 
 
+// A simple Addition with two constants works correctly.
   BinaryOperator* binOp = new Addition( new Constant( 11 ), new Constant( 5 ) );
 
   assert( binOp->toString() == "11+5" );
@@ -85,20 +97,41 @@ int main()
   delete addConst;
 
 
+
+// A simple Multiplication with two constants works correctly.
+  binOp = new Multiplication( new Constant( 11 ), new Constant( 5 ) );
+
+  assert( binOp->toString() == "11*5" );
+
+  addConst = dynamic_cast<Constant*>( binOp->simplify() );
+
+  assert( addConst != nullptr );
+  assert( addConst->getValue() == 55 );
+
+  constValue = binOp->evaluate();
+  assert( constValue == 55 );
+
+  delete binOp;
+  delete addConst;
+
+
+
+// A BinaryOperator (for instance an Addition) containing other
+// BinaryOperators works correctly.
   Addition* addOp =
     new Addition(
-      new Addition( new Constant( 5 ), new Constant( 1 ) ),
+      new Multiplication( new Constant( 5 ), new Constant( 1 ) ),
       new Constant( -1 )
     );
 
-  assert( addOp->toString() == "(5+1)+-1" );
+  assert( addOp->toString() == "(5*1)+-1" );
 
-  assert( addOp->evaluate() == 5 );
+  assert( addOp->evaluate() == 4 );
 
   addConst = dynamic_cast<Constant*>( addOp->simplify() );
 
   assert( addConst != nullptr );
-  assert( addConst->getValue() == 5 );
+  assert( addConst->getValue() == 4 );
 
   delete addOp;
   delete addConst;
@@ -113,6 +146,8 @@ int main()
 
   assert( addOp->toString() == "(5+var)+-1" );
 
+// Evaluating a BinaryOperator that contains a Variable without
+// specifying a value for the Variable throws a runtime_error.
   excThrown = false;
 
   try
@@ -126,9 +161,12 @@ int main()
 
   assert( excThrown == true );
 
+// Simplifying a BinaryOperator that contains a Variable without
+// specifying a value for the Variable returns a copy of the BinaryOperator.
   Addition* addSimpl = dynamic_cast<Addition*>( addOp->simplify() );
 
   assert( addSimpl != nullptr );
+  assert( addSimpl != addOp );
   assert( addSimpl->toString() == "(5+var)+-1" );
 
   varMap = Expression::VariableMap( { { "var", 50 } } );
@@ -140,20 +178,21 @@ int main()
 
 
 
+// A deep BinaryOperator with Variables works correctly.
   addOp =
     new Addition(
       new Addition(
-        new Addition( new Constant( 5 ), new Variable( "varA" ) ),
+        new Multiplication( new Constant( 5 ), new Variable( "varA" ) ),
         new Addition( new Constant( 7 ), new Variable( "varB" ) )
       ),
       new Constant( -1 )
     );
 
-  assert( addOp->toString() == "((5+varA)+(7+varB))+-1" );
+  assert( addOp->toString() == "((5*varA)+(7+varB))+-1" );
 
   addSimpl = dynamic_cast<Addition*>( addOp->simplify() );
   assert( addSimpl != nullptr );
-  assert( addSimpl->toString() == "((5+varA)+(7+varB))+-1" );
+  assert( addSimpl->toString() == "((5*varA)+(7+varB))+-1" );
 
   excThrown = false;
 
@@ -168,40 +207,23 @@ int main()
 
   assert( excThrown == true );
 
+// Simplifying a deep BinaryOperator with two Variables while specifying
+// a value for only one Variable works correctly.
   varMap = Expression::VariableMap( { { "varA", 50 } } );
 
   Expression* exprSimpl = addOp->simplify( varMap );
 
-  assert( exprSimpl->toString() == "(55+(7+varB))+-1" );
+  assert( exprSimpl->toString() == "(250+(7+varB))+-1" );
 
+// Evaluating while specifying values for both variables also works correctly.
   varMap = Expression::VariableMap( { { "varA", 50 }, { "varB", -6 } } );
 
-  assert( addOp->evaluate( varMap ) == 55 );
+  assert( addOp->evaluate( varMap ) == 250 );
 
   delete addOp;
   delete addSimpl;
   delete exprSimpl;
 
-
-
-  addOp =
-    new Addition(
-      new Addition(
-        new Addition(
-          new Addition( new Constant( 7 ), new Constant( 3 ) ),
-          new Variable( "varA" )
-        ),
-        new Addition( new Constant( 7 ), new Variable( "varB" ) )
-      ),
-      new Constant( -1 )
-    );
-
-  addSimpl = dynamic_cast<Addition*>( addOp->simplify() );
-  assert( addSimpl != nullptr );
-  assert( addSimpl->toString() == "((10+varA)+(7+varB))+-1" );
-
-  delete addOp;
-  delete addSimpl;
-
   return 0;
 }
+
