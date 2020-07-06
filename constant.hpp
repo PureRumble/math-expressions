2961781f48f6cab0ec0435c2e957b898b425b4f9
Arash Bazrafshan <arashu@gmail.com>
2020-07-06 17:16:32 +0200

    Add documentation for class Constant and its memb funcs.

--- constant.hpp
+++ constant.hpp
@@ -8,6 +8,9 @@
 namespace ImintMath
 {
 
+/* Class instance represents a constant in a mathematical expression.
+For instance new Addition( new Constant( 5 ), new Constant( 7 ) ) is 5+7.
+*/
 class Constant : public Expression
 {
   private:
@@ -20,12 +23,15 @@ class Constant : public Expression
 
     virtual std::string getStrRepr( bool withParantheses ) const override;
 
+/* Returns a copy of this Constant, irrespective of the arg map's content. */
     virtual Expression* simplifyRec( const Expression::VariableMap& map )
     const override;
 
+/* Returns the value of this constant. */
     Expression::ValueType getValue() const;
 };
 
 }
 
 #endif
+
